601c381a6788cf32f1ca8b4446481faa9974b84c
hauck <hauck@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2012-09-19 16:14:50 +0000

    Removed dependency to CIP completions, use workflow engine extension to add SimuCom tabs
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/PerOpteryx/trunk@16444 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.dsexplore.analysis.simucom/src/de/uka/ipd/sdq/dsexplore/analysis/simucom/launch/SimuComAnalysisTabGroup.java
+++ de.uka.ipd.sdq.dsexplore.analysis.simucom/src/de/uka/ipd/sdq/dsexplore/analysis/simucom/launch/SimuComAnalysisTabGroup.java
@@ -1,14 +1,17 @@
 package de.uka.ipd.sdq.dsexplore.analysis.simucom.launch;
 
-import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
-//import de.uka.ipd.sdq.cip.launchconfig.CompletionTab;
-import de.uka.ipd.sdq.cip.runtime.runconfig.tabs.CipCompletionTab;
 import de.uka.ipd.sdq.codegen.simucontroller.runconfig.FeatureOptionsTab;
 import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuComConfigurationTab;
 import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuConfigurationTab;
+import de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs.WorkflowHooks;
+import de.uka.ipd.sdq.workflow.launchconfig.extension.ExtendableTabGroup;
+//import de.uka.ipd.sdq.cip.launchconfig.CompletionTab;
 
  /**
   * Provides a configuration tab group for the SimuCom analysis method.
@@ -16,16 +19,17 @@ import de.uka.ipd.sdq.codegen.simucontroller.runconfig.SimuConfigurationTab;
   * @author pmerkle
   * 
   */
-public class SimuComAnalysisTabGroup extends AbstractLaunchConfigurationTabGroup {
+public class SimuComAnalysisTabGroup extends ExtendableTabGroup {
 
 	@Override
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
-		ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[] {
-				new SimuComConfigurationTab(), 
-				new SimuConfigurationTab(),
-				new FeatureOptionsTab(),
-				new CipCompletionTab(),
-		};
-		setTabs(tabs);
+		List<ILaunchConfigurationTab> tabs = new ArrayList<ILaunchConfigurationTab>();
+		tabs.add(new SimuComConfigurationTab());
+		tabs.add(new SimuConfigurationTab());
+		tabs.add(new FeatureOptionsTab());
+		for (String workflowExtensionPointId : WorkflowHooks.getAllWorkflowHookIDs()) {
+            tabs.addAll(createExtensionTabs(dialog, mode, workflowExtensionPointId));
+        }
+		setTabs(tabs.toArray(new ILaunchConfigurationTab[] {}));
 	}
 }
