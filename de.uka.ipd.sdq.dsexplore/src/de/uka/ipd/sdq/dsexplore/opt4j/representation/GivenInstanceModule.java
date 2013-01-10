4491b3c0d04502f55082497c349144266e417355
martens <martens@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2013-01-10 13:11:10 +0000

    also apply Guice fix from rev 17239 (branch) to GivenInstanceModule.java
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/PerOpteryx/trunk@17616 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/opt4j/representation/GivenInstanceModule.java
+++ de.uka.ipd.sdq.dsexplore/src/de/uka/ipd/sdq/dsexplore/opt4j/representation/GivenInstanceModule.java
@@ -1,7 +1,10 @@
 package de.uka.ipd.sdq.dsexplore.opt4j.representation;
 
+import org.opt4j.config.annotations.Info;
+import org.opt4j.config.annotations.Order;
 import org.opt4j.core.Genotype;
 import org.opt4j.core.IndividualFactory;
+import org.opt4j.core.optimizer.MaxIterations;
 import org.opt4j.core.optimizer.OptimizerModule;
 import org.opt4j.operator.copy.Copy;
 
@@ -12,6 +15,12 @@ import de.uka.ipd.sdq.dsexplore.opt4j.optimizer.PredefinedInstanceEvaluator;
 
 public class GivenInstanceModule extends OptimizerModule {
 
+	@Info("The number of generations.")
+	@Order(0)
+	@MaxIterations	
+	//not needed for this module, but otherwise Guice throws an error.
+	protected int generations = 1000;
+	
 	@Override
 	protected void config() {
 		bindOptimizer(PredefinedInstanceEvaluator.class);
@@ -20,6 +29,25 @@ public class GivenInstanceModule extends OptimizerModule {
 		
 		bind(new TypeLiteral<Copy<Genotype>>() {}).to((Class<? extends Copy<Genotype>>) CopyDesignDecisionGenotype.class);
 				
+	}
+	
+	/**
+	 * Returns the number of generations.
+	 * 
+	 * @return the number of generations
+	 */
+	public int getGenerations() {
+		return generations;
+	}
+	
+	/**
+	 * Sets the number of generations.
+	 * 
+	 * @param generations
+	 *            the number of generations
+	 */
+	public void setGenerations(int generations) {
+		this.generations = generations;
 	}
 
 }
