2e34fb7d475295dcee192143d60d652e5420e069
martens <martens@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2015-04-28 12:28:25 +0000

    updated feature xml which had an old dependency to simulizar. comment in Activator.java
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/PerOpteryx/trunk@26321 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- de.uka.ipd.sdq.dsexplore.analysis.simucom/src/de/uka/ipd/sdq/dsexplore/analysis/simucom/Activator.java
+++ de.uka.ipd.sdq.dsexplore.analysis.simucom/src/de/uka/ipd/sdq/dsexplore/analysis/simucom/Activator.java
@@ -5,6 +5,7 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
+ * copied from some other PCM plugin
  */
 public class Activator extends AbstractUIPlugin {
 
