6ef5583b11a2b5fa0fc4f8b18d718bbe2bfb907d
abusch <abusch@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2015-07-02 09:26:17 +0000

    moved
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/PerOpteryx/trunk@28091 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ de.uka.ipd.sdq.dsexplore.analysis.nqr/src/de/uka/ipd/sdq/dsexplore/analysis/nqr/NqrAnalysisTab.java
@@ -0,0 +1,118 @@
+package de.uka.ipd.sdq.dsexplore.analysis.nqr;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+
+import de.uka.ipd.sdq.dsexplore.launch.DSEConstantsContainer;
+import de.uka.ipd.sdq.workflow.launchconfig.LaunchConfigPlugin;
+import de.uka.ipd.sdq.workflow.launchconfig.tabs.TabHelper;
+
+public class NqrAnalysisTab extends de.uka.ipd.sdq.workflow.pcm.runconfig.FileNamesInputTab implements
+		ILaunchConfigurationTab {
+
+	private Text nqrModel;
+	
+//	private Button combineCostTypesInObjective;
+//	private Button considerInitialCost;
+//	private Button considerOperatingCost;
+
+	@Override
+	public void createControl(Composite parent) {
+		
+		// Create a listener for GUI modification events:
+		final ModifyListener modifyListener = new ModifyListener() {
+
+			public void modifyText(ModifyEvent e) {
+				NqrAnalysisTab.this.setDirty(true);
+				NqrAnalysisTab.this.updateLaunchConfigurationDialog();
+			}
+		};
+		
+		// Create a new Composite to hold the page's controls:
+		final Composite container = new Composite(parent, SWT.NONE);
+		setControl(container);
+		container.setLayout(new GridLayout());
+
+		/**
+		 * Add nqr model input section
+		 */
+		this.nqrModel = new Text(container, SWT.SINGLE | SWT.BORDER);
+		TabHelper.createFileInputSection(container, modifyListener, "Nqr Model File", DSEConstantsContainer.NQR_MODEL_EXTENSION, nqrModel, getShell(), "");
+		
+	}
+
+	@Override
+	public String getName() {
+		return "NQR Analysis";
+	}
+
+	@Override
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		try {
+			this.nqrModel.setText(configuration.getAttribute(
+					DSEConstantsContainer.NQR_FILE, ""));
+		} catch (CoreException e) {
+			LaunchConfigPlugin.errorLogger(getName(),DSEConstantsContainer.NQR_FILE, e.getMessage());
+		}
+		
+	}
+
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		configuration.setAttribute(
+				DSEConstantsContainer.NQR_FILE, 
+				this.nqrModel.getText());
+	}
+
+	@Override
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+				
+	}
+
+	@Override
+	public void activated(ILaunchConfigurationWorkingCopy workingCopy) {
+		// Leave this method empty to prevent unnecessary invocation of
+		// initializeFrom() and multiple resulting invocations of
+		// performApply().
+		
+	}
+
+	@Override
+	public void deactivated(ILaunchConfigurationWorkingCopy workingCopy) {
+				
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();		
+	}
+
+	@Override
+	public Image getImage() {
+		return null;
+	}
+
+	@Override
+	public boolean isValid(ILaunchConfiguration launchConfig) {
+		String extension = DSEConstantsContainer.NQR_MODEL_EXTENSION[0].replace("*", "");
+		//if (this.nqrModel.getText().equals("") || !this.nqrModel.getText().contains(extension)){
+		//	setErrorMessage("NQR model is missing!");
+		//	return false;
+		//}
+		return true;
+	}
+
+
+
+}
---
+++ de.uka.ipd.sdq.pcm.nqr/src/de/uka/ipd/sdq/nqr/impl/NqrRepositoryImpl.java
@@ -0,0 +1,152 @@
+/**
+ */
+package de.uka.ipd.sdq.nqr.impl;
+
+import de.uka.ipd.sdq.nqr.Nqr;
+import de.uka.ipd.sdq.nqr.NqrPackage;
+import de.uka.ipd.sdq.nqr.NqrRepository;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Repository</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link de.uka.ipd.sdq.nqr.impl.NqrRepositoryImpl#getNqr <em>Nqr</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class NqrRepositoryImpl extends MinimalEObjectImpl.Container implements NqrRepository {
+	/**
+	 * The cached value of the '{@link #getNqr() <em>Nqr</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getNqr()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<Nqr> nqr;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NqrRepositoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return NqrPackage.Literals.NQR_REPOSITORY;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EList<Nqr> getNqr() {
+		if (nqr == null) {
+			nqr = new EObjectContainmentEList<Nqr>(Nqr.class, this, NqrPackage.NQR_REPOSITORY__NQR);
+		}
+		return nqr;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+			case NqrPackage.NQR_REPOSITORY__NQR:
+				return ((InternalEList<?>)getNqr()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case NqrPackage.NQR_REPOSITORY__NQR:
+				return getNqr();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case NqrPackage.NQR_REPOSITORY__NQR:
+				getNqr().clear();
+				getNqr().addAll((Collection<? extends Nqr>)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case NqrPackage.NQR_REPOSITORY__NQR:
+				getNqr().clear();
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case NqrPackage.NQR_REPOSITORY__NQR:
+				return nqr != null && !nqr.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
+
+} //NqrRepositoryImpl
