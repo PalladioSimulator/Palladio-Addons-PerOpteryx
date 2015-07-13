bae1dcc924fd966319b1388a11a377769af96724
abusch <abusch@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2015-07-13 13:25:38 +0000

    GDOF branch

---
+++ de.uka.ipd.sdq.pcm.taskmodel/src/de/uka/ipd/sdq/pcm/taskmodel/util/taskmodelAdapterFactory.java
@@ -0,0 +1,210 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package de.uka.ipd.sdq.pcm.taskmodel.util;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+import org.eclipse.emf.ecore.EObject;
+
+import de.uka.ipd.sdq.pcm.taskmodel.Alternative;
+import de.uka.ipd.sdq.pcm.taskmodel.AlternativeTask;
+import de.uka.ipd.sdq.pcm.taskmodel.Edge;
+import de.uka.ipd.sdq.pcm.taskmodel.Task;
+import de.uka.ipd.sdq.pcm.taskmodel.TaskList;
+import de.uka.ipd.sdq.pcm.taskmodel.taskmodelPackage;
+
+/**
+ * <!-- begin-user-doc --> The <b>Adapter Factory</b> for the model. It provides an adapter
+ * <code>createXXX</code> method for each class of the model. <!-- end-user-doc -->
+ *
+ * @see de.uka.ipd.sdq.pcm.taskmodel.taskmodelPackage
+ * @generated
+ */
+public class taskmodelAdapterFactory extends AdapterFactoryImpl {
+    /**
+     * The cached model package. <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    protected static taskmodelPackage modelPackage;
+
+    /**
+     * Creates an instance of the adapter factory. <!-- begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    public taskmodelAdapterFactory() {
+        if (modelPackage == null)
+        {
+            modelPackage = taskmodelPackage.eINSTANCE;
+        }
+    }
+
+    /**
+     * Returns whether this factory is applicable for the type of the object. <!-- begin-user-doc
+     * --> This implementation returns <code>true</code> if the object is either the model's package
+     * or is an instance object of the model. <!-- end-user-doc -->
+     *
+     * @return whether this factory is applicable for the type of the object.
+     * @generated
+     */
+    @Override
+    public boolean isFactoryForType(final Object object) {
+        if (object == modelPackage)
+        {
+            return true;
+        }
+        if (object instanceof EObject)
+        {
+            return ((EObject) object).eClass().getEPackage() == modelPackage;
+        }
+        return false;
+    }
+
+    /**
+     * The switch that delegates to the <code>createXXX</code> methods. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     *
+     * @generated
+     */
+    protected taskmodelSwitch<Adapter> modelSwitch =
+            new taskmodelSwitch<Adapter>()
+            {
+                @Override
+                public Adapter caseTask(final Task object)
+                {
+                    return taskmodelAdapterFactory.this.createTaskAdapter();
+                }
+
+                @Override
+                public Adapter caseEdge(final Edge object)
+                {
+                    return taskmodelAdapterFactory.this.createEdgeAdapter();
+                }
+
+                @Override
+                public Adapter caseTaskList(final TaskList object)
+                {
+                    return taskmodelAdapterFactory.this.createTaskListAdapter();
+                }
+
+                @Override
+                public Adapter caseAlternative(final Alternative object)
+                {
+                    return taskmodelAdapterFactory.this.createAlternativeAdapter();
+                }
+
+                @Override
+                public Adapter caseAlternativeTask(final AlternativeTask object)
+                {
+                    return taskmodelAdapterFactory.this.createAlternativeTaskAdapter();
+                }
+
+                @Override
+                public Adapter defaultCase(final EObject object)
+                {
+                    return taskmodelAdapterFactory.this.createEObjectAdapter();
+                }
+            };
+
+    /**
+     * Creates an adapter for the <code>target</code>. <!-- begin-user-doc --> <!-- end-user-doc -->
+             *
+             * @param target
+             *            the object to adapt.
+             * @return the adapter for the <code>target</code>.
+     * @generated
+     */
+    @Override
+    public Adapter createAdapter(final Notifier target) {
+        return this.modelSwitch.doSwitch((EObject) target);
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link de.uka.ipd.sdq.pcm.taskmodel.Task
+             * <em>Task</em>}'. <!-- begin-user-doc --> This default implementation returns null so that we
+             * can easily ignore cases; it's useful to ignore a case when inheritance will catch all the
+             * cases anyway. <!-- end-user-doc -->
+             *
+             * @return the new adapter.
+     * @see de.uka.ipd.sdq.pcm.taskmodel.Task
+     * @generated
+     */
+    public Adapter createTaskAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link de.uka.ipd.sdq.pcm.taskmodel.Edge
+             * <em>Edge</em>}'. <!-- begin-user-doc --> This default implementation returns null so that we
+             * can easily ignore cases; it's useful to ignore a case when inheritance will catch all the
+             * cases anyway. <!-- end-user-doc -->
+             *
+             * @return the new adapter.
+     * @see de.uka.ipd.sdq.pcm.taskmodel.Edge
+     * @generated
+     */
+    public Adapter createEdgeAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '{@link de.uka.ipd.sdq.pcm.taskmodel.TaskList
+             * <em>Task List</em>}'. <!-- begin-user-doc --> This default implementation returns null so
+             * that we can easily ignore cases; it's useful to ignore a case when inheritance will catch all
+             * the cases anyway. <!-- end-user-doc -->
+             *
+             * @return the new adapter.
+     * @see de.uka.ipd.sdq.pcm.taskmodel.TaskList
+     * @generated
+     */
+    public Adapter createTaskListAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '
+             * {@link de.uka.ipd.sdq.pcm.taskmodel.Alternative <em>Alternative</em>}'. <!-- begin-user-doc
+             * --> This default implementation returns null so that we can easily ignore cases; it's useful
+             * to ignore a case when inheritance will catch all the cases anyway. <!-- end-user-doc -->
+             *
+             * @return the new adapter.
+     * @see de.uka.ipd.sdq.pcm.taskmodel.Alternative
+     * @generated
+     */
+    public Adapter createAlternativeAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for an object of class '
+             * {@link de.uka.ipd.sdq.pcm.taskmodel.AlternativeTask <em>Alternative Task</em>}'. <!--
+             * begin-user-doc --> This default implementation returns null so that we can easily ignore
+             * cases; it's useful to ignore a case when inheritance will catch all the cases anyway. <!--
+             * end-user-doc -->
+             *
+             * @return the new adapter.
+     * @see de.uka.ipd.sdq.pcm.taskmodel.AlternativeTask
+     * @generated
+     */
+    public Adapter createAlternativeTaskAdapter() {
+        return null;
+    }
+
+    /**
+     * Creates a new adapter for the default case. <!-- begin-user-doc --> This default
+             * implementation returns null. <!-- end-user-doc -->
+             *
+             * @return the new adapter.
+     * @generated
+     */
+    public Adapter createEObjectAdapter() {
+        return null;
+    }
+
+} // taskmodelAdapterFactory
