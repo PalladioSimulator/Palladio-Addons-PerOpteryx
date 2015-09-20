68dc0fbc01842a9f8da8315f66204de556212b7a
apoorv_patwardhan <apoorv_patwardhan@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2015-09-20 04:10:22 +0000

    Added more comments to code for scoring networks. Specifically, added comments to K2 scoring mechanism. The comments are applicable to other scoring procedures too ...

--- de.uka.ipd.sdq.dsexplore.bayesnets/src/de/uka/ipd/sdq/dsexplore/bayesnets/utility/BayesNetworkScore.java
+++ de.uka.ipd.sdq.dsexplore.bayesnets/src/de/uka/ipd/sdq/dsexplore/bayesnets/utility/BayesNetworkScore.java
@@ -118,7 +118,11 @@ public static void main(String[] args){
 }
 
 // Start writing methods here
-
+// If you want to have an idea of how the score 
+// calculation is coded, please see the K2NetworkScore()
+// method in detail. It contains some comments and clarifications
+// which will make digestion of the code easier and faster. All other 
+// methods are coded in the same way.
 
 /**
  * Scores the network according to the K2 scoring technique
@@ -136,22 +140,42 @@ public double K2NetworkScore(){
 	int n = Data[1].length; // No. of nodes/variables
 	
 	int r = 2; // Number of states of the random variable Xi
+	// Currently, only binary variables are considered. Hence,
+	// for every variable Xi, the no. of states are 2.
 	
 	// Prepare matrix q. q[i] is the number of configurations which parents of
 	// Xi can take.
 	BayesNetwork BN = new BayesNetwork(n);
-	int[] q = new int[n];
+	int[] q = new int[n]; // initialize q array.
 	for(int i = 0;i<n;i++){
+		// for every Xi variable, calculate q[i].
+		// q[i] = r^(No. of parents of Xi)
 		q[i] = (int) Math.pow(r,(BN.getParents(Graph, i)).length);
 	}
 	
 	// create the w matrix which stores the configurations for the parents 
-	// of node Xi
+	// of node Xi. A heads up: it is stored in a little different way than 
+	// which normally/ automatically comes to the mind. w.get(i) will
+	// fetch the ith variable's (i.e Xi's) matrix. This matrix stores 
+	// the various configurations of its parents. However, note that
+	// a row represents a data variable, while a column represents
+	// a configuration of the parents. Thus, the matrix will have dimensions
+	// :- (No. of parents of variable Xi)*(2^(No. of parents of variable Xi))
+	
 	//int[][][] w = new int[n][n][(int) Math.pow(2, n)];
-	ArrayList<int[][]> w = new ArrayList<int[][]>();
-	for(int i = 0;i< n;i++){
+	ArrayList<int[][]> w = new ArrayList<int[][]>(); // Initialize w 
+	for(int i = 0;i< n;i++){ // Keep in mind - n is no. of variables/Xis  
 		// Create the wij matrix
 		int[][] w_inter = new int[BN.getParents(Graph, i).length][(int) Math.pow(2, BN.getParents(Graph, i).length)];
+		// w_inter corresponds to Xi's matrix.
+		// <--------------------------------------------------------------------->
+		// This code between the dotted lines
+		// fills in the w_inter matrix with all possible
+		// configurations. For example, 1st column is the 1st
+		// configuration (all zeroes), 2nd column is the 2nd config
+		// and so on. The ordering of parents is top-down. Which 
+		// means, the uppermost row contains the most significant bits
+		// while the lowermost row contains the least significant bits.
 		for(int j = 0;j< (BN.getParents(Graph, i)).length;j++){
 			int Threshold=(int) Math.pow(2,(BN.getParents(Graph, i)).length-(j+1));
 			int count = 1;
@@ -169,6 +193,7 @@ public double K2NetworkScore(){
 				count++;
 			}
 		}
+		// <---------------------------------------------------------------------->
 		w.add(w_inter);
 	}
 	// Created and stored the various configurations pertaining to various Xi nodes
@@ -176,9 +201,12 @@ public double K2NetworkScore(){
 	
 	// Now again create the 3 Dimensional matrix Nijk
 	//int[][][] N = new int[n][(int) Math.pow(2, n)][r];
+	// N.get(i) will give a matrix that corresponds to Nijk.
+	// Specifically, N.get(i)[j][k] will be equal to Nijk.
 	ArrayList<int[][]> N = new ArrayList<int[][]>();
-	for(int i = 0;i< n;i++){
-		int[][] N_inter = new int[q[i]][r];
+	for(int i = 0;i< n;i++){ // n:- No. of data variables
+		int[][] N_inter = new int[q[i]][r]; 
+		// The following for loop fills up N_inter appropriately.
 		for(int j = 0;j< q[i];j++){
 			for(int k=0;k< r;k++){
 				// Create a matrix for parents of node i (Xi)
