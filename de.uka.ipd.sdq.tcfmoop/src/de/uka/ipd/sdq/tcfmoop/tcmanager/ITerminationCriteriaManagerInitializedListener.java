92e092cf0289df14fe3c33a7b6b16527005739b2
s_fswobo <s_fswobo@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2015-10-12 14:23:27 +0000

    delete project content

--- de.uka.ipd.sdq.tcfmoop/src/de/uka/ipd/sdq/tcfmoop/tcmanager/ITerminationCriteriaManagerInitializedListener.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ipd.sdq.tcfmoop.tcmanager;
-
-public interface ITerminationCriteriaManagerInitializedListener {
-	
-	/**
-	 * Logic witch should be executed when a TerminationCriteriaManagerInitialized is fired.
-	 */
-	public void handleTerminationCriteriaManagerInitializedEvent(ITerminationCriteriaManager instance);
-}
