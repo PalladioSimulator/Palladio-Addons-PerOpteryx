760c50417c69fb017123725d34fbacfb13bd2cb5
s_fswobo <s_fswobo@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-01-20 10:53:10 +0000

    brought emf generated code up to date PART 5

--- de.uka.ipd.sdq.pcm.designdecision.editor/src/de/uka/ipd/sdq/pcm/designdecision/presentation/DesignDecisionEditorPlugin.java
+++ de.uka.ipd.sdq.pcm.designdecision.editor/src/de/uka/ipd/sdq/pcm/designdecision/presentation/DesignDecisionEditorPlugin.java
@@ -19,90 +19,84 @@ import de.uka.ipd.sdq.pcm.cost.provider.CostModelEditPlugin;
 import de.uka.ipd.sdq.probfunction.provider.ProbabilityFunctionEditPlugin;
 import de.uka.ipd.sdq.stoex.provider.StoexEditPlugin;
 import de.uka.ipd.sdq.units.provider.UnitsEditPlugin;
+import genericdesigndecision.provider.GenericdesigndecisionEditPlugin;
 
 /**
- * This is the central singleton for the DesignDecision editor plugin. <!-- begin-user-doc --> <!--
+ * This is the central singleton for the DesignDecision editor plugin.
+ * <!-- begin-user-doc --> <!--
  * end-user-doc -->
- * 
  * @generated
  */
 public final class DesignDecisionEditorPlugin extends EMFPlugin {
-    /**
-     * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    public static final DesignDecisionEditorPlugin INSTANCE = new DesignDecisionEditorPlugin();
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static final DesignDecisionEditorPlugin INSTANCE = new DesignDecisionEditorPlugin();
 
-    /**
-     * Keep track of the singleton. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    private static Implementation plugin;
+	/**
+	 * Keep track of the singleton.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static Implementation plugin;
 
-    /**
-     * Create the instance. <!-- begin-user-doc --> <!-- end-user-doc -->
-     * 
-     * @generated
-     */
-    public DesignDecisionEditorPlugin() {
-        super(new ResourceLocator[]
-                {
-                CostModelEditPlugin.INSTANCE,
-                EcoreEditPlugin.INSTANCE,
-                FeaturemodelEditPlugin.INSTANCE,
-                IdentifierEditPlugin.INSTANCE,
-                PalladioComponentModelEditPlugin.INSTANCE,
-                ResultdecoratorEditPlugin.INSTANCE,
-                StoexEditPlugin.INSTANCE,
-                UnitsEditPlugin.INSTANCE,
-                ProbabilityFunctionEditPlugin.INSTANCE,
-                });
-    }
+	/**
+	 * Create the instance.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public DesignDecisionEditorPlugin() {
+		super(new ResourceLocator[] { CostModelEditPlugin.INSTANCE, EcoreEditPlugin.INSTANCE,
+				FeaturemodelEditPlugin.INSTANCE, GenericdesigndecisionEditPlugin.INSTANCE,
+				IdentifierEditPlugin.INSTANCE, PalladioComponentModelEditPlugin.INSTANCE,
+				ProbabilityFunctionEditPlugin.INSTANCE, ResultdecoratorEditPlugin.INSTANCE, StoexEditPlugin.INSTANCE,
+				UnitsEditPlugin.INSTANCE, });
+	}
 
-    /**
-     * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @return the singleton instance.
-     * @generated
-     */
-    @Override
-    public ResourceLocator getPluginResourceLocator() {
-        return plugin;
-    }
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	@Override
+	public ResourceLocator getPluginResourceLocator() {
+		return plugin;
+	}
 
-    /**
-     * Returns the singleton instance of the Eclipse plugin. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @return the singleton instance.
-     * @generated
-     */
-    public static Implementation getPlugin() {
-        return plugin;
-    }
+	/**
+	 * Returns the singleton instance of the Eclipse plugin.
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * @return the singleton instance.
+	 * @generated
+	 */
+	public static Implementation getPlugin() {
+		return plugin;
+	}
 
-    /**
-     * The actual implementation of the Eclipse <b>Plugin</b>. <!-- begin-user-doc --> <!--
-     * end-user-doc -->
-     * 
-     * @generated
-     */
-    public static class Implementation extends EclipseUIPlugin {
-        /**
-         * Creates an instance. <!-- begin-user-doc --> <!-- end-user-doc -->
-         * 
-         * @generated
-         */
-        public Implementation() {
-            super();
+	/**
+	 * The actual implementation of the Eclipse <b>Plugin</b>.
+	 * <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * @generated
+	 */
+	public static class Implementation extends EclipseUIPlugin {
+		/**
+		 * Creates an instance.
+		 * <!-- begin-user-doc --> <!-- end-user-doc -->
+		 * @generated
+		 */
+		public Implementation() {
+			super();
 
-            // Remember the static instance.
-            //
-            plugin = this;
-        }
-    }
+			// Remember the static instance.
+			//
+			plugin = this;
+		}
+	}
 
 }
