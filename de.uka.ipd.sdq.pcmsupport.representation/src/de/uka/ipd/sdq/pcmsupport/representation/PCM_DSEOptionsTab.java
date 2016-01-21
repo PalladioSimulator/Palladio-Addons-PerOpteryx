351e9d7e7a1722cd2a0caa30f6fb000880b02493
s_fswobo <s_fswobo@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-01-21 20:00:02 +0000

    moved pcm-specific tabs

---
+++ de.uka.ipd.sdq.pcmsupport.representation/src/de/uka/ipd/sdq/pcmsupport/representation/PCM_DSEOptionsTab.java
@@ -0,0 +1,42 @@
+package de.uka.ipd.sdq.pcmsupport.representation;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.swt.widgets.Composite;
+
+import de.uka.ipd.sdq.dsexplore.launch.DSEConstantsContainer;
+import de.uka.ipd.sdq.dsexplore.launch.DSEOptionsTab;
+import de.uka.ipd.sdq.pcmsupport.designdecision.MetamodelDescription;
+import de.uka.ipd.sdq.workflow.launchconfig.LaunchConfigPlugin;
+import genericdesigndecision.universalDoF.Metamodel;
+import genericdesigndecision.universalDoF.SpecificDoF;
+
+public class PCM_DSEOptionsTab extends DSEOptionsTab {
+
+	@Override
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		
+		for (SpecificDoF s : MetamodelDescription.eINSTANCE.listSDoFs()) {
+			specificDofList.add(s.getName());
+		}
+	}
+	
+	@Override 
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		super.initializeFrom(configuration);
+		try {
+			this.metamodel = Metamodel.get(configuration.getAttribute(DSEConstantsContainer.INPUT_METAMODEL, 0));
+		} catch (CoreException e) {
+			LaunchConfigPlugin.errorLogger(getName(), DSEConstantsContainer.INPUT_METAMODEL, e.getMessage());
+		}
+	}
+	
+	@Override
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		super.performApply(configuration);
+		configuration.setAttribute(DSEConstantsContainer.INPUT_METAMODEL, this.metamodel.getValue());
+	}
+
+}
