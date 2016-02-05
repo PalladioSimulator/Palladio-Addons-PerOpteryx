0d2379773fa9a178f14a22342d4d0e25c7b18941
s_fswobo <s_fswobo@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-02-05 23:10:14 +0000

    added comment

--- de.uka.ipd.sdq.pcmsupport.representation/src/de/uka/ipd/sdq/pcmsupport/representation/PCM_DSEOptionsTab.java
+++ de.uka.ipd.sdq.pcmsupport.representation/src/de/uka/ipd/sdq/pcmsupport/representation/PCM_DSEOptionsTab.java
@@ -10,14 +10,21 @@ import de.uka.ipd.sdq.workflow.launchconfig.LaunchConfigPlugin;
 import genericdesigndecision.universalDoF.Metamodel;
 import genericdesigndecision.universalDoF.SpecificDoF;
 
+/**
+ * This class is a subclass of the generic DSEOptionsTab and describes additional, pcm-specific dse options
+ * @author ferdinand
+ *
+ */
 public class PCM_DSEOptionsTab extends DSEOptionsTab {
 
-	@Override 
-	// enables early warning if launch configuration does not fit UI tab
+	/**
+	 * basically does the same as the super-method but enables early warning if launch configuration does not fit UI tab
+	 */
+	@Override
 	public void initializeFrom(ILaunchConfiguration configuration) {
 		try {
 			if(configuration.getAttribute(DSEConstantsContainer.INPUT_METAMODEL, Metamodel.PCM_VALUE) != Metamodel.PCM_VALUE) {
-				throw new IllegalStateException("This representation tab is not intended to be used with metamodel of the launch configuration.");
+				throw new IllegalStateException("This representation tab is not intended to be used with the metamodel of the launch configuration.");
 			}
 		} catch (CoreException e) {
 			LaunchConfigPlugin.errorLogger(getName(), DSEConstantsContainer.INPUT_METAMODEL, e.getMessage());
