a461b619289746b71d72f30730f0004ffed5d2f9
s_fswobo <s_fswobo@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-02-11 11:23:01 +0000

    references to AGenomeToCandidateModelTransformation have changed part 2

--- de.uka.ipd.sdq.dsexplore.genericdesigndecision.edit/src/genericdesigndecision/provider/AGenomeToCandidateModelTransformationItemProvider.java
+++ de.uka.ipd.sdq.dsexplore.genericdesigndecision.edit/src/genericdesigndecision/provider/AGenomeToCandidateModelTransformationItemProvider.java
@@ -20,7 +20,7 @@ import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
 /**
- * This is the item provider adapter for a {@link genericdesigndecision.AGenomeToCandidateModelTransformation} object.
+ * This is the item provider adapter for a {@link genericdesigndecision.universalDoF.AGenomeToCandidateModelTransformation} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
  * @generated
