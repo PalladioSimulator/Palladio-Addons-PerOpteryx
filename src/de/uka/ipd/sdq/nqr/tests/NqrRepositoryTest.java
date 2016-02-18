1be51e2ac9edb81e1ffea47343dfca00c5a918c2
abusch <abusch@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-02-18 10:00:15 +0000

    nqr

---
+++ src/de/uka/ipd/sdq/nqr/tests/NqrRepositoryTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package de.uka.ipd.sdq.nqr.tests;
+
+import de.uka.ipd.sdq.nqr.NqrFactory;
+import de.uka.ipd.sdq.nqr.NqrRepository;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Repository</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class NqrRepositoryTest extends TestCase {
+
+	/**
+	 * The fixture for this Repository test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NqrRepository fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(NqrRepositoryTest.class);
+	}
+
+	/**
+	 * Constructs a new Repository test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NqrRepositoryTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Repository test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(NqrRepository fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Repository test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected NqrRepository getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(NqrFactory.eINSTANCE.createNqrRepository());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //NqrRepositoryTest
