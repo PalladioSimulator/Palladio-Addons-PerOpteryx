fcbb3f5eae8908e5010cd1a096741adaa12438e1
s_fswobo <s_fswobo@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-02-18 13:00:47 +0000

    repaired call to EMFHelper

--- de.uka.ipd.sdq.dsexplore.analysis.cost/src/de/uka/ipd/sdq/dsexplore/analysis/cost/CostEvaluator.java
+++ de.uka.ipd.sdq.dsexplore.analysis.cost/src/de/uka/ipd/sdq/dsexplore/analysis/cost/CostEvaluator.java
@@ -345,7 +345,7 @@ public class CostEvaluator extends AbstractAnalysis implements IAnalysis{
 	 */
 	private CostRepository getCostModel(DSEWorkflowConfiguration configuration) throws CoreException {
 		String costModelFileName = configuration.getRawConfiguration().getAttribute(DSEConstantsContainer.COST_FILE, "");
-		CostRepository cr =  (CostRepository)EMFHelper.loadFromXMIFile(costModelFileName, costPackage.eINSTANCE);
+		CostRepository cr =  (CostRepository)de.uka.ipd.sdq.pcmsupport.helper.EMFHelper.loadFromXMIFile(costModelFileName, costPackage.eINSTANCE);
 		if (cr == null){
 			throw new CoreException(new Status(Status.ERROR, "de.uka.ipd.sdq.dsexplore", 0, "Cost model "+costModelFileName+" could not be loaded.", null));
 		}
