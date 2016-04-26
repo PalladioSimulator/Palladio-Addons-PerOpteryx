ced78bfdf586e28e3b77495ac761c232968d3197
daniel_sachsenmaier <daniel_sachsenmaier@769cc9a0-fb0c-0410-8c2f-a2305f0e6268>
2016-04-26 21:58:11 +0000

    clean up

--- de.uka.ipd.sdq.pcm.nqr.edit/de.uka.ipd.sdq.dsexplore.qml.handling/src/de/uka/ipd/sdq/dsexplore/qml/pcm/datastructures/EntryLevelSystemCallCriterion.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ipd.sdq.dsexplore.qml.pcm.datastructures;
-
-import org.palladiosimulator.pcm.usagemodel.EntryLevelSystemCall;
-
-public interface EntryLevelSystemCallCriterion {
-
-	public EntryLevelSystemCall getEntryLevelSystemCall();
-	
-}
