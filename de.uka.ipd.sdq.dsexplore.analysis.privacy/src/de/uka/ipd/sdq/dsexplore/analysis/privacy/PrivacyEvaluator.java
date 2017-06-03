c583cf618f58df0b2ccc63339a008be6a72a55ae
Philipp Weimann <teigerweimann@web.de>
2017-06-03 11:28:58 +0200

    minor changes at PrivacyEvaluator

--- de.uka.ipd.sdq.dsexplore.analysis.privacy/src/de/uka/ipd/sdq/dsexplore/analysis/privacy/PrivacyEvaluator.java
+++ de.uka.ipd.sdq.dsexplore.analysis.privacy/src/de/uka/ipd/sdq/dsexplore/analysis/privacy/PrivacyEvaluator.java
@@ -1,5 +1,8 @@
 package de.uka.ipd.sdq.dsexplore.analysis.privacy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.opt4j.core.Criterion;
@@ -16,6 +19,8 @@ import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 
 public class PrivacyEvaluator extends AbstractAnalysis implements IAnalysis {
+
+	private Map<Long, PrivacyAnalysisResult> previousPrivacyResults = new HashMap<Long, PrivacyAnalysisResult>();
 
 	public PrivacyEvaluator(IAnalysisQualityAttributeDeclaration qualityAttribute) {
 		super(qualityAttribute);
@@ -37,19 +42,18 @@ public class PrivacyEvaluator extends AbstractAnalysis implements IAnalysis {
 
 	@Override
 	public IAnalysisResult retrieveResultsFor(PCMPhenotype pheno, Criterion criterion) throws CoreException, AnalysisFailedException {
-		// TODO Auto-generated method stub
-		return null;
+		PrivacyAnalysisResult result = this.previousPrivacyResults.get(pheno.getNumericID());
+		return result;
 	}
 
 	@Override
 	public boolean hasObjectivePerUsageScenario() throws CoreException {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void setBlackboard(MDSDBlackboard blackboard) {
-		// TODO Auto-generated method stub
+		this.blackboard = blackboard;
 	}
 
 }
