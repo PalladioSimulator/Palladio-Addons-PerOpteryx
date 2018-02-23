52c78a1e0f60a1a7be924022e48ae57953255360
Yves Schneider <yves.schneider@student.kit.edu>
2018-02-23 12:52:13 +0100

    Sprache und Auswertung für die QualityEffectSpecification erweitert

--- de.uka.ipd.sdq.dsexplore.analysis.nqr/src/de/uka/ipd/sdq/dsexplore/analysis/qes/QesFactory.java
+++ de.uka.ipd.sdq.dsexplore.analysis.nqr/src/de/uka/ipd/sdq/dsexplore/analysis/qes/QesFactory.java
@@ -2,11 +2,18 @@ package de.uka.ipd.sdq.dsexplore.analysis.qes;
 
 import static org.eclipse.core.runtime.IStatus.ERROR;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.StringJoiner;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
+import org.palladiosimulator.pcm.core.composition.Connector;
+import org.palladiosimulator.pcm.core.composition.impl.AssemblyConnectorImpl;
+import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import org.palladiosimulator.qes.qualityEffectSpecification.FindByName;
 import org.palladiosimulator.qes.qualityEffectSpecification.Model;
 import de.uka.ipd.sdq.dsexplore.analysis.PCMPhenotype;
 import de.uka.ipd.sdq.dsexplore.analysis.nqr.solver.NqrFactory;
@@ -17,8 +24,7 @@ public class QesFactory extends NqrFactory {
 
     private static final Logger LOG = Logger.getLogger("de.uka.ipd.sdq.dsexplore.analysis.qes");
 
-    private static Model loadModel(final String qualityEffectSpecificationUri)
-            throws CoreException {
+    private static Model loadModel(final String qualityEffectSpecificationUri) throws CoreException {
         try {
             URI loadFrom = URI.createURI(qualityEffectSpecificationUri);
             if (!loadFrom.isPlatform()) {
@@ -27,20 +33,17 @@ public class QesFactory extends NqrFactory {
             return new QesParser().getModel(loadFrom);
         } catch (final Exception e) {
             throw new CoreException(new Status(ERROR, "de.uka.ipd.sdq.dsexplore.analysis.qes", 0,
-                    "Qes Model " + String.valueOf(qualityEffectSpecificationUri)
-                            + " could not be loaded.",
-                    e));
+                    "Qes Model " + String.valueOf(qualityEffectSpecificationUri) + " could not be loaded.", e));
         }
 
 
     }
 
     private final Model model;
-    private List<String> effectedComponents;
+    private Set<String> effectedComponents;
 
-    public QesFactory(final String reasoningComponentUri, final String reductionUri,
-            final String dimensionTypeSetUri, final String qualityEffectSpecificationUri)
-            throws CoreException {
+    public QesFactory(final String reasoningComponentUri, final String reductionUri, final String dimensionTypeSetUri,
+            final String qualityEffectSpecificationUri) throws CoreException {
         super(reasoningComponentUri, reductionUri, dimensionTypeSetUri);
 
         model = loadModel(qualityEffectSpecificationUri);
@@ -51,9 +54,28 @@ public class QesFactory extends NqrFactory {
             return;
         }
 
-        effectedComponents = new ArrayList<>();
+        effectedComponents = new HashSet<>();
 
-        // TODO Auto-generated method stub
+        for (final FindByName byName : model.getElements()) {
+            final String name = byName.getName();
+            for (final Connector c : pheno.getPCMInstance().getSystem().getConnectors__ComposedStructure()) {
+                if ((c != null) && (c instanceof AssemblyConnectorImpl)) {
+                    final AssemblyConnectorImpl ac = (AssemblyConnectorImpl) c;
+
+                    equalsName(name, ac.basicGetProvidingAssemblyContext_AssemblyConnector()
+                            .getEncapsulatedComponent__AssemblyContext());
+
+                    equalsName(name, ac.getRequiringAssemblyContext_AssemblyConnector()
+                            .getEncapsulatedComponent__AssemblyContext());
+                }
+            }
+        }
+    }
+
+    private void equalsName(final String name, final RepositoryComponent component) {
+        if (component.getEntityName().equalsIgnoreCase(name)) {
+            effectedComponents.add(component.getId());
+        }
     }
 
     @Override
@@ -64,7 +86,7 @@ public class QesFactory extends NqrFactory {
 
         final List<NqrProxy> nqrs = new ArrayList<>(super.getNqrList(componentId));
 
-        // TODO Auto-generated method stub
+        LOG.error("Is effected: " + getComponentName(componentId));
         return nqrs;
     }
 
@@ -74,16 +96,35 @@ public class QesFactory extends NqrFactory {
             return super.getReasoningList(componentId);
         }
 
-        final List<ReasoningProxy> reasonings =
-                new ArrayList<>(super.getReasoningList(componentId));
+        final List<ReasoningProxy> reasonings = new ArrayList<>(super.getReasoningList(componentId));
 
-        // TODO Auto-generated method stub
-        return null;
+        LOG.error("Is effected: " + getComponentName(componentId));
+        return reasonings;
     }
 
     private boolean isEffected(final String componentId) {
-        return (effectedComponents != null) && (componentId != null)
-                && effectedComponents.contains(componentId);
+        return (effectedComponents != null) && (componentId != null) && effectedComponents.contains(componentId);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(super.toString());
+        builder.append("Effected Components [");
+        if (effectedComponents != null && !effectedComponents.isEmpty()) {
+            final StringJoiner joiner = new StringJoiner(", ");
+            effectedComponents.forEach(component -> joiner.add(getComponentName(component)));
+        }
+        builder.append("]").append(System.lineSeparator());
+        builder.append("Model (").append(String.valueOf(model)).append(") ]").append(System.lineSeparator());
+        if (model != null && model.getElements() != null && !model.getElements().isEmpty()) {
+            for (Object element : model.getElements()) {
+                builder.append("\t").append(String.valueOf(element)).append(System.lineSeparator());
+            }
+        }
+        builder.append("]").append(System.lineSeparator());
+        return builder.toString();
+
     }
 
 }
