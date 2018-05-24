ff22b438728e0558844a05b5572e6ab6b3286133
Dominik Fuchß <develop@fuchss.org>
2018-05-24 18:19:51 +0200

    Determine Available vs Provided CVs

--- edu.kit.ipd.are.dsexplore.featurecompletions.weaver/src/edu/kit/ipd/are/dsexplore/featurecompletions/weaver/FCCWeaver.java
+++ edu.kit.ipd.are.dsexplore.featurecompletions.weaver/src/edu/kit/ipd/are/dsexplore/featurecompletions/weaver/FCCWeaver.java
@@ -4,15 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.palladiosimulator.analyzer.workflow.blackboard.PCMResourceSetPartition;
+import org.palladiosimulator.pcm.core.composition.Connector;
+import org.palladiosimulator.pcm.core.entity.Entity;
+import org.palladiosimulator.pcm.repository.Interface;
+import org.palladiosimulator.pcm.repository.OperationInterface;
+import org.palladiosimulator.pcm.repository.OperationSignature;
 import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryComponent;
+import org.palladiosimulator.pcm.system.System;
 import org.palladiosimulator.solver.models.PCMInstance;
 
+import FeatureCompletionModel.ComplementumVisnetis;
+import de.uka.ipd.sdq.dsexplore.tools.primitives.Pair;
 import de.uka.ipd.sdq.dsexplore.tools.repository.MergedRepository;
 import de.uka.ipd.sdq.dsexplore.tools.stereotypeapi.StereotypeAPIHelper;
 import de.uka.ipd.sdq.pcm.cost.CostRepository;
 import de.uka.ipd.sdq.pcm.designdecision.Choice;
 import edu.kit.ipd.are.dsexplore.featurecompletions.weaver.port.FCCWeaverException;
 import edu.kit.ipd.are.dsexplore.featurecompletions.weaver.strategy.IWeavingStrategy;
+import edu.kit.ipd.are.dsexplore.featurecompletions.weaver.strategy.WeavingInstruction;
 import edu.kit.ipd.are.dsexplore.featurecompletions.weaver.strategy.WeavingStrategies;
 import featureSolution.InclusionMechanism;
 
@@ -56,7 +66,64 @@ public final class FCCWeaver {
 	}
 
 	public PCMInstance getWeavedInstance(PCMInstance original) {
+
+		List<WeavingInstruction> instructions = this.determineInstructions(original);
+
 		return original;
+	}
+
+	private List<WeavingInstruction> determineInstructions(PCMInstance original) {
+		System pcmSystem = original.getSystem();
+		List<Pair<Connector, ComplementumVisnetis>> availableCVs = this.extractAvailableCVs(pcmSystem);
+		List<Pair<Entity, ComplementumVisnetis>> providedCVs = this.extractProvidedCVs();
+
+		return null;
+	}
+
+	private List<Pair<Connector, ComplementumVisnetis>> extractAvailableCVs(System pcmSystem) {
+		List<Pair<Connector, ComplementumVisnetis>> result = new ArrayList<>();
+
+		for (Connector c : pcmSystem.getConnectors__ComposedStructure()) {
+			List<ComplementumVisnetis> cv = StereotypeAPIHelper.getViaStereoTypeFrom(c, ComplementumVisnetis.class, "target");
+			if (cv.isEmpty()) {
+				continue;
+			}
+			result.add(Pair.of(c, cv.get(0)));
+		}
+
+		return result;
+	}
+
+	private List<Pair<Entity, ComplementumVisnetis>> extractProvidedCVs() {
+		List<Pair<Entity, ComplementumVisnetis>> result = new ArrayList<>();
+
+		for (Repository pcmRepo : this.mergedRepo) {
+			for (RepositoryComponent rc : pcmRepo.getComponents__Repository()) {
+				List<ComplementumVisnetis> cvsRc = StereotypeAPIHelper.getViaStereoTypeFrom(rc, ComplementumVisnetis.class, "fulfillsComplementumVisnetis");
+				for (ComplementumVisnetis cv : cvsRc) {
+					result.add(Pair.of(rc, cv));
+				}
+			}
+			for (Interface iface : pcmRepo.getInterfaces__Repository()) {
+				if (!(iface instanceof OperationInterface)) {
+					continue;
+				}
+				OperationInterface opIface = (OperationInterface) iface;
+				List<ComplementumVisnetis> cvsIface = StereotypeAPIHelper.getViaStereoTypeFrom(opIface, ComplementumVisnetis.class, "fulfillsComplementumVisnetis");
+				for (ComplementumVisnetis cv : cvsIface) {
+					result.add(Pair.of(opIface, cv));
+				}
+
+				for (OperationSignature opSig : opIface.getSignatures__OperationInterface()) {
+					List<ComplementumVisnetis> cvsSig = StereotypeAPIHelper.getViaStereoTypeFrom(opSig, ComplementumVisnetis.class, "fulfillsComplementumVisnetis");
+					for (ComplementumVisnetis cv : cvsSig) {
+						result.add(Pair.of(opSig, cv));
+					}
+				}
+			}
+		}
+
+		return result;
 	}
 
 	public MergedRepository getMergedRepo() {
