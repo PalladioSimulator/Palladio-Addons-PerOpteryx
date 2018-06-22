770709ac3997aaa0a38563e7be65a94366eba94e
Maximilian Peters <de.maxptrs@googlemail.com>
2018-06-22 16:54:06 +0200

    maintainability extention

--- de.uka.ipd.sdq.dsexplore.qml.handling/src/de/uka/ipd/sdq/dsexplore/qml/handling/QMLConstantsContainer.java
+++ de.uka.ipd.sdq.dsexplore.qml.handling/src/de/uka/ipd/sdq/dsexplore/qml/handling/QMLConstantsContainer.java
@@ -5,20 +5,18 @@ package de.uka.ipd.sdq.dsexplore.qml.handling;
 
 /**
  * Due to cyclic dependencies, QML needs its own constants container
+ * 
  * @author noorshams
  *
  */
 public class QMLConstantsContainer {
 
-
-	//	public static final String STANDARD_CONTRACT_TYPE_PATH =
-	//		"pathmap://PCM_MODELS/PCMStandardQMLContractType.qmldeclarations";
-
+	// public static final String STANDARD_CONTRACT_TYPE_PATH =
+	// "pathmap://PCM_MODELS/PCMStandardQMLContractType.qmldeclarations";
 
 	public static final String QUALITY_ATTRIBUTE_DIMENSION_TOTAL_COST_DEFINITION_PATH = "pathmap://PCM_MODELS/Dimension_cost.qmlcontracttype";
 	public static final String QUALITY_ATTRIBUTE_DIMENSION_INITIAL_COST_DEFINITION_PATH = "pathmap://PCM_MODELS/Dimension_initialcost.qmlcontracttype";
 	public static final String QUALITY_ATTRIBUTE_DIMENSION_OPERATING_COST_DEFINITION_PATH = "pathmap://PCM_MODELS/Dimension_operatingcost.qmlcontracttype";
-
 
 	public static final String QUALITY_ATTRIBUTE_DIMENSION_POFOD_DEFINITION_PATH = "pathmap://PCM_MODELS/Dimension_pofod.qmlcontracttype";
 	public static final String QUALITY_ATTRIBUTE_DIMENSION_RESPONSETIME_DEFINITION_PATH = "pathmap://PCM_MODELS/Dimension_responsetime.qmlcontracttype";
@@ -27,13 +25,12 @@ public class QMLConstantsContainer {
 
 	// added:
 	public static final String QUALITY_ATTRIBUTE_DIMENSION_SECURITY_PATH = "platform:/plugin/edu.kit.ipd.are.dsexplore.analysis.security/Security.qmlcontracttype";
+	public static final String QUALITY_ATTRIBUTE_DIMENSION_MAINTAINABILITY_PATH = "platform:/plugin/de.uka.ipd.sdq.dsexplore.analysis.maintainability/Maintainability.qmlcontracttype";
 
 	public static final String[] QUALITY_ATTRIBUTE_DIMENSION_DEFINITION_PATHS = new String[] {
 			QUALITY_ATTRIBUTE_DIMENSION_RESPONSETIME_DEFINITION_PATH,
-			QUALITY_ATTRIBUTE_DIMENSION_THROUGHPUT_DEFINITION_PATH,
-			QUALITY_ATTRIBUTE_DIMENSION_POFOD_DEFINITION_PATH,
-			QUALITY_ATTRIBUTE_DIMENSION_TOTAL_COST_DEFINITION_PATH, QUALITY_ATTRIBUTE_DIMENSION_SECURITY_PATH
-	};
+			QUALITY_ATTRIBUTE_DIMENSION_THROUGHPUT_DEFINITION_PATH, QUALITY_ATTRIBUTE_DIMENSION_POFOD_DEFINITION_PATH,
+			QUALITY_ATTRIBUTE_DIMENSION_TOTAL_COST_DEFINITION_PATH, QUALITY_ATTRIBUTE_DIMENSION_SECURITY_PATH };
 
 	// quick version, delete later
 	// public static final String QUALITY_ATTRIBUTE_DIMENSION_SECURITY_PATH =
