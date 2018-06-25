c266a2e1700cff4ec89fb3935a8c5d98b69b5345
Dominik Fuchß <develop@fuchss.org>
2018-06-25 23:07:08 +0200

    Fixed Naming bug in PerformanceAnalysis

--- de.uka.ipd.sdq.dsexplore.tools/src/de/uka/ipd/sdq/dsexplore/tools/repository/MergedRepository.java
+++ de.uka.ipd.sdq.dsexplore.tools/src/de/uka/ipd/sdq/dsexplore/tools/repository/MergedRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.emf.ecore.impl.EObjectImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 import org.palladiosimulator.pcm.repository.BasicComponent;
 import org.palladiosimulator.pcm.repository.ComponentType;
 import org.palladiosimulator.pcm.repository.ProvidedRole;
@@ -29,43 +28,44 @@ import de.uka.ipd.sdq.pcm.cost.CostRepository;
  */
 public final class MergedRepository extends EObjectImpl implements Iterable<Repository> {
 	private final Set<Repository> repositories;
-	private Repository repoWithoutAnnotations;
+	// private Repository repoWithoutAnnotations;
 
 	public MergedRepository(Repository... repositories) {
 		this.repositories = new HashSet<>();
 		this.repositories.addAll(Arrays.asList(repositories));
-		this.createRepo();
+		// this.createRepo();
 	}
 
 	public MergedRepository(List<Repository> repositories) {
 		this.repositories = new HashSet<>();
 		this.repositories.addAll(repositories);
-		this.createRepo();
+		// this.createRepo();
 	}
 
 	public MergedRepository(Set<Repository> repositories) {
 		this.repositories = new HashSet<>();
 		this.repositories.addAll(repositories);
-		this.createRepo();
+		// this.createRepo();
 	}
 
-	private void createRepo() {
-		Repository repo = RepositoryFactory.eINSTANCE.createRepository();
+	// private void createRepo() {
+	// Repository repo = RepositoryFactory.eINSTANCE.createRepository();
+	// repo.setEntityName("MergedRepo");
+	// repo.setId("MergedRepo" + this.hashCode());
+	// for (Repository r : this.repositories) {
+	// Repository copy = this.copyOf(r);
+	// repo.getComponents__Repository().addAll(copy.getComponents__Repository());
+	// }
+	// this.repoWithoutAnnotations = repo;
+	//
+	// }
 
-		for (Repository r : this.repositories) {
-			Repository copy = this.copyOf(r);
-			repo.getComponents__Repository().addAll(copy.getComponents__Repository());
-		}
-		this.repoWithoutAnnotations = repo;
-
-	}
-
-	private Repository copyOf(Repository repo) {
-		Copier copier = new Copier();
-		Repository newRepo = (Repository) copier.copy(repo);
-		copier.copyReferences();
-		return newRepo;
-	}
+	// private Repository copyOf(Repository repo) {
+	// Copier copier = new Copier();
+	// Repository newRepo = (Repository) copier.copy(repo);
+	// copier.copyReferences();
+	// return newRepo;
+	// }
 
 	public List<CostRepository> getCostRepos() {
 		Set<CostRepository> repos = new HashSet<>();
@@ -111,9 +111,9 @@ public final class MergedRepository extends EObjectImpl implements Iterable<Repo
 	 *            - The name of the adapter component.
 	 * @return the created adapter component.
 	 */
-	public RepositoryComponent createAndAddAdapter(String name) {
+	public RepositoryComponent createAndAddAdapter(String name, Repository repo) {
 		BasicComponent adapter = this.createAdapter(name);
-		this.addAdapter(adapter);
+		repo.getComponents__Repository().add(adapter);
 		return adapter;
 	}
 
@@ -124,13 +124,13 @@ public final class MergedRepository extends EObjectImpl implements Iterable<Repo
 		return adapter;
 	}
 
-	private void addAdapter(BasicComponent adapter) {
-		this.repoWithoutAnnotations.getComponents__Repository().add(adapter);
-	}
+	// private void addAdapter(BasicComponent adapter) {
+	// this.repoWithoutAnnotations.getComponents__Repository().add(adapter);
+	// }
 
-	public Repository getAsRepoWithoutStereotypes() {
-		return this.repoWithoutAnnotations;
-	}
+	// public Repository getAsRepoWithoutStereotypes() {
+	// return this.repoWithoutAnnotations;
+	// }
 
 	public List<ProvidedRole> getAllProvidedRoles() {
 		List<ProvidedRole> prs = new ArrayList<>();
