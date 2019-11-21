01ab7e64ba6f5e03540702835263e3ec45231060
Dominik Fuchß <develop@fuchss.org>
2019-11-21 11:32:19 +0100

    Added PathChangerJob

--- bundles/de.uka.ipd.sdq.dsexplore.analysis.simulizar/src/de/uka/ipd/sdq/dsexplore/analysis/simulizar/SimulizarJob.java
+++ bundles/de.uka.ipd.sdq.dsexplore.analysis.simulizar/src/de/uka/ipd/sdq/dsexplore/analysis/simulizar/SimulizarJob.java
@@ -1,6 +1,7 @@
 package de.uka.ipd.sdq.dsexplore.analysis.simulizar;
 
 import org.eclipse.core.runtime.CoreException;
+import org.palladiosimulator.simulizar.launcher.jobs.PCMInterpreterRootCompositeJob;
 import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
 
 import de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs.AbstractSimulationJob;
@@ -8,7 +9,7 @@ import de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs.AbstractSimulationJob
 public class SimulizarJob extends AbstractSimulationJob<SimuLizarWorkflowConfiguration> {
 
 	public SimulizarJob(final SimuLizarWorkflowConfiguration configuration, int counter) throws CoreException {
-		super(SimulizarJob.updateConfig(configuration, counter));
+		super(SimulizarJob.updateConfig(configuration, counter), null, false);
 
 	}
 
@@ -19,6 +20,7 @@ public class SimulizarJob extends AbstractSimulationJob<SimuLizarWorkflowConfigu
 
 	@Override
 	protected void addSimulatorSpecificJobs(SimuLizarWorkflowConfiguration configuration) {
-		this.add(new CustomPCMInterpreterRootCompositeJob(configuration));
+		this.add(new PathChangerJob(configuration));
+		this.add(new PCMInterpreterRootCompositeJob(configuration));
 	}
 }
