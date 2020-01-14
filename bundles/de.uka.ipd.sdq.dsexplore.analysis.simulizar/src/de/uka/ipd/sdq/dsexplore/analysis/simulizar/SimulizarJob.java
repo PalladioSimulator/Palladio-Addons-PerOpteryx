e8d07fa927ae9d6bbd5c4bd26d951a9687d09c47
Dominik Fuchß <develop@fuchss.org>
2020-01-14 21:19:42 +0100

    Fixed issue regarding disappearing resource set partitions by disabling cleanup mechanisms for generated models.

--- bundles/de.uka.ipd.sdq.dsexplore.analysis.simulizar/src/de/uka/ipd/sdq/dsexplore/analysis/simulizar/SimulizarJob.java
+++ bundles/de.uka.ipd.sdq.dsexplore.analysis.simulizar/src/de/uka/ipd/sdq/dsexplore/analysis/simulizar/SimulizarJob.java
@@ -1,10 +1,12 @@
 package de.uka.ipd.sdq.dsexplore.analysis.simulizar;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.palladiosimulator.simulizar.launcher.jobs.PCMInterpreterRootCompositeJob;
 import org.palladiosimulator.simulizar.runconfig.SimuLizarWorkflowConfiguration;
 
 import de.uka.ipd.sdq.codegen.simucontroller.workflow.jobs.AbstractSimulationJob;
+import de.uka.ipd.sdq.workflow.jobs.CleanupFailedException;
 
 /**
  * Defines the job which runs SimuLizar on the current PCM Model of PerOpteryx
@@ -46,5 +48,10 @@ public class SimulizarJob extends AbstractSimulationJob<SimuLizarWorkflowConfigu
 	protected void addSimulatorSpecificJobs(SimuLizarWorkflowConfiguration configuration) {
 		this.add(new PathChangerJob(configuration));
 		this.add(new PCMInterpreterRootCompositeJob(configuration));
+	}
+
+	@Override
+	public void cleanup(IProgressMonitor monitor) throws CleanupFailedException {
+		// Disable cleanup for issues with missing partitions..
 	}
 }
